37ab9c555be7dc20e471034261a9464c56f9dbe7
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-06 17:27:55 +0545

    Added SignUp and SignIn Page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,27 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+import '../globals.css';
 
 export const metadata = {
-    title:'Threads',
-    description:"A Next.js 13 Meta Threads Application"
+    title: 'Threads',
+    description: "A Next.js 13 Meta Threads Application"
 }
 
-export default function RootLayout({children}:{children:React.ReactNode}) {
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <head>
+
+                </head>
+                <body className={`${inter.className} bg-dark-1`}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    )
 
 }
