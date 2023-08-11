77d1fcb8aad41b132cba5d6f9850dc74bd8590b6
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-11 18:40:38 +0545

    Worked

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,144 +1,203 @@
-"use client"
+"use client";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod';
-import { UserValidation } from '@/lib/validations/user';
-import * as z from 'zod';
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserValidation } from "@/lib/validations/user";
+import * as z from "zod";
 import { Button } from "../ui/button";
 import Image from "next/image";
 import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { isBase64Image } from "@/lib/utils";
-import { useUploadThing } from '@/lib/uploadthing'
-
+import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
-    user: {
-        id: string,
-        username: string,
-        name: string,
-        bio: string,
-        image: string
-    },
-    btnTitle: string
+  user: {
+    id: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
 }
-
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const { startUpload } = useUploadThing("media");
 
-    const [files, setFiles] = useState<File[]>([]);
-    const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
-    const form = useForm({
-        resolver: zodResolver(UserValidation),
-        defaultValues: {
-            profile_photo: user?.image ? user.image : "",
-            name: user?.name ? user.name : "",
-            username: user?.username ? user.username : "",
-            bio: user?.bio ? user.bio : "",
-        }
-    })
+  const form = useForm({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: user?.image ? user.image : "",
+      name: user?.name ? user.name : "",
+      username: user?.username ? user.username : "",
+      bio: user?.bio ? user.bio : "",
+    },
+  });
 
-    function onHandleImage(e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) {
-        e.preventDefault();
+  function onHandleImage(
+    e: ChangeEvent<HTMLInputElement>,
+    fieldChange: (value: string) => void
+  ) {
+    e.preventDefault();
 
-        const fileReader = new FileReader();
+    const fileReader = new FileReader();
 
-        if (e.target.files && e.target.files.length > 0) {
-            const file = e.target.files[0];
-            setFiles(Array.from(e.target.files));
-            if (!file.type.includes('image')) {
-                return;
-            }
-            fileReader.onload = async (event) => {
-                const imageDataURL = event?.target?.result?.toString() || "";
-                fieldChange(imageDataURL);
-            }
-            fileReader.readAsDataURL(file)
-        }
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      setFiles(Array.from(e.target.files));
+      if (!file.type.includes("image")) {
+        return;
+      }
+      fileReader.onload = async (event) => {
+        const imageDataURL = event?.target?.result?.toString() || "";
+        fieldChange(imageDataURL);
+      };
+      fileReader.readAsDataURL(file);
     }
+  }
 
-    async function onSubmit(values: z.infer<typeof UserValidation>) {
-        const blob = values.profile_photo;
-        const hasImageChanged = isBase64Image(blob);
-        if (hasImageChanged) {
-            const imgRes = await startUpload(files);
-            if (imgRes && imgRes[0].fileUrl) {
-                values.profile_photo = imgRes[0].fileUrl;
-            }
-        }
-        
-
+  async function onSubmit(values: z.infer<typeof UserValidation>) {
+    const blob = values.profile_photo;
+    const hasImageChanged = isBase64Image(blob);
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files);
+      if (imgRes && imgRes[0].fileUrl) {
+        values.profile_photo = imgRes[0].fileUrl;
+      }
     }
+    await updateUser({
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      userId: user.id,
+      path: pathname,
+    });
+  }
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-10 justify-start">
-                <FormField
-                    control={form.control}
-                    name="profile_photo"
-                    render={({ field }) => (
-                        <FormItem className="flex  items-center gap-4">
-                            <FormLabel className="account-form_image-label">{
-                                field.value ? (
-                                    <Image src={field.value} alt="Profile Photo" width={96} height={96} priority className="rounded-full object-contain" />
-                                ) : <Image src={"/assets/profile.svg"} alt="Profile Photo" width={24} height={24} className="object-contain" />
-                            }</FormLabel>
-                            <FormControl className="flex-1 text-base-semibold text-gray-100">
-                                <Input type="file" accept="image/*" placeholder="Upload a photo" className="account-form_image-input" onChange={(e) => {
-                                    onHandleImage(e, field.onChange);
-                                }} />
-                            </FormControl>
-                        </FormItem>
-                    )}
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-10 justify-start"
+      >
+        <FormField
+          control={form.control}
+          name="profile_photo"
+          render={({ field }) => (
+            <FormItem className="flex  items-center gap-4">
+              <FormLabel className="account-form_image-label">
+                {field.value ? (
+                  <Image
+                    src={field.value}
+                    alt="Profile Photo"
+                    width={96}
+                    height={96}
+                    priority
+                    className="rounded-full object-contain"
+                  />
+                ) : (
+                  <Image
+                    src={"/assets/profile.svg"}
+                    alt="Profile Photo"
+                    width={24}
+                    height={24}
+                    className="object-contain"
+                  />
+                )}
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-100">
+                <Input
+                  type="file"
+                  accept="image/*"
+                  placeholder="Upload a photo"
+                  className="account-form_image-input"
+                  onChange={(e) => {
+                    onHandleImage(e, field.onChange);
+                  }}
                 />
-                <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem className="flex flex-col gap-3 w-full">
-                            <FormLabel className="text-base-semibold text-light-2">Name</FormLabel>
-                            <FormControl className="flex-1 text-base-semibold text-gray-100">
-                                <Input className="account-form_input no-focus" type="text"   {...field} />
-                            </FormControl>
-                        </FormItem>
-                    )}
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Name
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-100">
+                <Input
+                  className="account-form_input no-focus"
+                  type="text"
+                  {...field}
                 />
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem className="flex flex-col gap-3 w-full">
-                            <FormLabel className="text-base-semibold text-light-2">Username</FormLabel>
-                            <FormControl className="flex-1 text-base-semibold text-gray-100">
-                                <Input className="account-form_input no-focus" type="text"   {...field} />
-                            </FormControl>
-                        </FormItem>
-                    )}
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Username
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-100">
+                <Input
+                  className="account-form_input no-focus"
+                  type="text"
+                  {...field}
                 />
-                <FormField
-                    control={form.control}
-                    name="bio"
-                    render={({ field }) => (
-                        <FormItem className="flex flex-col gap-3 w-full">
-                            <FormLabel className="text-base-semibold text-light-2">Bio</FormLabel>
-                            <FormControl className="flex-1 text-base-semibold text-gray-100">
-                                <Textarea rows={5} className="account-form_input no-focus" placeholder="Hi I am,..."   {...field} />
-                            </FormControl>
-                        </FormItem>
-                    )}
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Bio
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-100">
+                <Textarea
+                  rows={5}
+                  className="account-form_input no-focus"
+                  placeholder="Hi I am,..."
+                  {...field}
                 />
-                <Button type="submit" className="bg-primary-500 hover:bg-[#8291d4] text-gray-900">Submit</Button>
-            </form>
-        </Form>
-    )
-}
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="bg-primary-500 hover:bg-[#8291d4] text-gray-900"
+        >
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
 
 export default AccountProfile;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,1 +1,46 @@
-"use server"
+"use server";
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.models";
+import { connectToDB } from "../mongoose";
+
+interface Params {
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+}
+
+export async function updateUser({
+  userId,
+  username,
+  name,
+  bio,
+  image,
+  path,
+}: Params): Promise<void> {
+  connectToDB();
+  try {
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name: name,
+        image,
+        bio,
+        onboarded: true,
+      },
+      {
+        upsert: true,
+      }
+    );
+    //Next JS function
+    if (path == "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    console.log(`A new error has occured on user action ${error.message}`);
+  }
+}
