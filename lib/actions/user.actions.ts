53f6d2932d8c51b6e40064ba3ad359958494a5ea
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-12 11:27:24 +0545

    Working on fetchUser Action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -22,7 +22,39 @@ export async function updateUser({
   path,
 }: Params): Promise<void> {
   try {
-    connectToDB();
+    connectToDB();  
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name: name,
+        image,
+        bio,
+        onboarded: true,
+      },
+      {
+        upsert: true,
+      }
+    );
+    //Next JS function
+    if (path == "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    console.log(`A new error has occured on user action ${error.message}`);
+  }
+}
+
+export async function fetchUser({
+  userId,
+  username,
+  name,
+  bio,
+  image,
+  path,
+}: Params): Promise<void> {
+  try {
+    connectToDB();  
     await User.findOneAndUpdate(
       { id: userId },
       {
