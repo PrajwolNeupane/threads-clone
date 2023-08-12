698503c5b0c3d77d8b15ea03919f652990be39bd
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-12 11:44:13 +0545

    Working on Create Thread Form

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,12 +1,26 @@
+import PostThread from "@/components/forms/PostThread";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { userInfo } from "os";
 
+async function Page() {
+  const user = await currentUser();
 
-async function Page(){
+  if (!user) null;
 
-    const user = await currentUser();
-
-    return <h1 className="head-text">Create Thread</h1>
+  if (typeof user?.id != "undefined") {
+    const userInfo = await fetchUser({ userId: user.id });
+    if (!userInfo?.onboarded) redirect("/onboarding");
+    else {
+      return (
+        <>
+          <h1 className="head-text">Create Thread</h1>
+          <PostThread userId={userInfo?._id} />
+        </>
+      );
+    }
+  }
 }
 
 export default Page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -45,33 +45,14 @@ export async function updateUser({
   }
 }
 
-export async function fetchUser({
-  userId,
-  username,
-  name,
-  bio,
-  image,
-  path,
-}: Params): Promise<void> {
+export async function fetchUser({userId}:{userId:string}): Promise<any> {
   try {
     connectToDB();  
-    await User.findOneAndUpdate(
-      { id: userId },
-      {
-        username: username.toLowerCase(),
-        name: name,
-        image,
-        bio,
-        onboarded: true,
-      },
-      {
-        upsert: true,
-      }
-    );
-    //Next JS function
-    if (path == "/profile/edit") {
-      revalidatePath(path);
-    }
+    return await User.findOne({id:userId})
+    // .populate({
+    //   path:'communities',
+    //   modal:"communities"
+    // })
   } catch (error: any) {
     console.log(`A new error has occured on user action ${error.message}`);
   }
