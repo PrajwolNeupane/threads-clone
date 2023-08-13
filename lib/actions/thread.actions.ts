99e451eae72598d1fc84b66971ea47d310e9d9e4
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-13 14:37:38 +0545

    Completed Thread Card UI

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -53,26 +53,25 @@ export async function fetchPost(pageNumber = 1, pageSize = 20) {
   }
 }
 
-export async function createThread({
-  text,
-  author,
-  communityId,
-  path,
-}: Params) {
-  try {
-    connectToDB();
-    const createdThread = await Thread.create({
-      text,
-      author,
-      community: null,
-    });
-    await User.findByIdAndUpdate(author, {
-      $push: { threads: createdThread._id },
-    });
-    revalidatePath(path);
-  } catch (error: any) {
-    console.log(
-      `A new error has occured on  createThread action ${error.message}`
-    );
+export async function createThread({ text, author, communityId, path }: Params
+  ) {
+    try {
+      connectToDB();
+  
+      const createdThread = await Thread.create({
+        text,
+        author,
+        community: null, // Assign communityId if provided, or leave it null for personal account
+      });
+  
+      // Update User model
+      await User.findByIdAndUpdate(author, {
+        $push: { threads: createdThread._id },
+      });
+  
+      revalidatePath(path);
+    } catch (error: any) {
+      throw new Error(`Failed to create thread: ${error.message}`);
+    }
   }
-}
+
