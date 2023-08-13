b8525e8225872322d90f4164fee50d0be8b2ba68
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-13 12:32:51 +0545

    Working on Thread Card UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
-//app/page.tsx
-import { UserButton } from "@clerk/nextjs";
-//<UserButton afterSignOutUrl="/"/>
-export default function Home() {
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPost } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
+
+export default async function Home() {
+
+  const result = await fetchPost(1,30);
+  const user = await currentUser();
+
   return (
-    <div>
+    <>
       <h1 className="head-text text-left">Home</h1>
-    </div>
+      <section className="mt-9 flex flex-col gap-10">
+        {
+          result?.post.length === 0 ? <p className="no-result">No Threads Found</p>  :
+          result?.post.map((curr,indx) => (
+            <ThreadCard key={indx} id={curr?._id} currentUserId={user?.id!} parentId={curr?.parentId} content={curr?.text} author={curr?.author} community={curr?.community} createdAt={curr?.createdAt} comments={curr?.comments}/>
+          ))
+        }
+      </section>
+    </>
   )
 } 
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -12,6 +12,47 @@ interface Params {
   path: string;
 }
 
+export async function fetchPost(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const postQuery = Thread.find({
+      parentId: { $in: [null, undefined] },
+    })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: "User",
+          select: "__id name parentId image",
+        },
+      });
+
+    const totalPostCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+
+    const post = await postQuery.exec();
+
+    const isNext = totalPostCount > skipAmount + post.length;
+
+    return {
+      post,isNext
+    }
+
+  } catch (error: any) {
+    console.log(
+      `A new error has occured on  createThread action ${error.message}`
+    );
+  }
+}
+
 export async function createThread({
   text,
   author,
