a64f8a9756c833e8a7fdeacbf91ca1f5ddf10891
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-14 19:46:37 +0545

    Working with UserCard

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,4 +1,5 @@
-import { fetchUser } from "@/lib/actions/user.actions";
+import UserCard from "@/components/cards/UserCard";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
@@ -11,9 +12,28 @@ async function page() {
     const userInfo = await fetchUser({ userId: user.id });
     if (!userInfo?.onboarded) redirect("/onboarding");
     else {
+      const result = await fetchUsers({
+        userId: user.id,
+        searchString: "",
+        pageNumber: 1,
+        pageSize: 25,
+      });
+      console.log(result);
+
       return (
         <section>
           <h1 className="head-text mb-10">Search</h1>
+          <div className="mt-14 flex flex-col gap-9">
+            {result?.users.length === 0 ? (
+              <p className="no-result">No Users</p>
+            ) : (
+              <>{
+                result?.users.map((user) => (
+                    <UserCard />
+                ))
+              }</>
+            )}
+          </div>
         </section>
       );
     }
---
+++ components/cards/UserCard.tsx
@@ -0,0 +1,14 @@
+
+interface Props {
+  id: string;
+  name: string;
+  username: string;
+  image: string;
+  personType: string;
+}
+
+function UserCard({ id, name, username, image, personType }: Props) {
+  return <></>;
+}
+
+export default UserCard;
