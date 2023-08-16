6528d2bd4fbf69e267df8002f3efc32a7344b624
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-16 23:31:13 +0545

    Added Search Functionality

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,58 +1,62 @@
-import { fetchUser, getActivity } from "@/lib/actions/user.actions";
-import { currentUser } from "@clerk/nextjs";
-import Image from "next/image";
-import Link from "next/link";
 import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
 
-async function page() {
+import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
+
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
+  if (!user) return null;
 
-  if (!user) null;
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-  if (typeof user?.id != "undefined") {
-    const userInfo = await fetchUser(user.id);
-    if (!userInfo?.onboarded) redirect("/onboarding");
-    else {
-      const activity = await getActivity(userInfo?._id);
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
 
-      return (
-        <section>
-          <h1 className="head-text mb-10">Communities</h1>
-          <section className="mt-10 flex flex-col gap-5">
-            {activity?.length! > 0 ? (
-              <>
-                {activity?.map((curr) => (
-                  <Link key={curr?._id} href={`/thread/${curr.parentId}`}>
-                    <article className="activity-card">
-                      <Image
-                        src={curr.author.image}
-                        alt="Profile Picture"
-                        width={30}
-                        height={20}
-                        className="rounded-full object-cover"
-                      />
-                      <p className="!text-small-regular text-light-1">
-                        <span className="mr-1 text-primary-500">
-                          {
-                            curr.author.name
-                          }
-                        </span>
-                        <span>
-                          {" "}replied to your thread
-                        </span>
-                      </p>
-                    </article>
-                  </Link>
-                ))}
-              </>
-            ) : (
-              <p className="!text-base-regular text-light-3">No activity yet</p>
-            )}
-          </section>
-        </section>
-      );
-    }
-  }
+  return (
+    <section>
+      <h1 className='head-text mb-10'>Search</h1>
+
+      <Searchbar routeType='search' />
+
+      <div className='mt-14 flex flex-col gap-9'>
+        {result.users.length === 0 ? (
+          <p className='no-result'>No Result</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                image={person.image}
+                personType='User'
+              />
+            ))}
+          </>
+        )}
+      </div>
+
+      <Pagination
+        path='search'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </section>
+  );
 }
 
-export default page;
+export default Page;
